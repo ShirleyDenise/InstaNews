21ac3291042b784d9ae5c6c0bdb9149ccc56e72a
Shirley <ma_shirley@msn.com>
2017-01-25 21:44:36 -0800

    js/jquery file fixed.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-$(function() {
-
-    // this is my message...
-    var message = 'Hello';
-    console.log(message);
+$(document).ready(function() {
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "bdae6f74955c4b3297d70a7d23446bad"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
 })
