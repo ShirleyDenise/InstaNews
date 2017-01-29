c5dedf79584c2fc18cfc2d79c002f58e709bda85
Shirley <ma_shirley@msn.com>
2017-01-29 11:57:05 -0800

    loading gif.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 $(document).ready(function() {
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  var userSelect = $('#sections').val();
+  var url = 'https://api.nytimes.com/svc/topstories/v2/' + userSelect + '.json';
 url += '?' + $.param({
-  'api-key': "bdae6f74955c4b3297d70a7d23446bad"
+  'api-key': "bdae6f74955c4b3297d70a7d23446bad",
+  'callback': "12"
 });
 $.ajax({
   url: url,
   method: 'GET',
+  dataType: 'Json'
 }).done(function(result) {
   console.log(result);
-}).fail(function(err) {
-  throw err;
 });
-})
+
+
+
+
+
+  $('#sections').on('change', function(){
+    //when section selected
+    event.preventDefault();
+    $('#img').show();
+    // show loading giff when loading section
+
+    // success:function(result){
+    //    $('#img').hide(); 
+    //    // hide loading gif
+  
+    // }
+  });
+
+  
+});
+
+
+    // var $dataSet = result.results.filter(function (item) {
+    //      return item.multimedia.length;
+    //    }).splice(0, 12);
